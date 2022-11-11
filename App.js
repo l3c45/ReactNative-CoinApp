2d73319a337422378b4b281acd586d9ffabc16ff
Lucas <mrlucaszarate@gmail.com>
2022-11-11 18:34:52 -0300

    Modificar estilos

--- App.js
+++ App.js
@@ -1,11 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Keyboard,
+  Alert,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { APIRequest } from "./utils/API";
+import { useEffect, useState } from "react";
+import Item from "./components/Item";
+
+import { Icon } from "@rneui/base";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [update, setUpdate] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [search, setSearch] = useState(false);
+
+  const getData = async () => {
+    const data = await APIRequest();
+    setData(data);
+  };
+
+  useEffect(() => {
+    getData();
+    
+  }, []);
+
+  const handeTextInput = (input) => {
+    setSearchTerm(input.toLowerCase());
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.title}>CryptoApp</Text>
+        {search ? (
+          <TextInput
+            onBlur={(e) => setSearch(false)}
+            onSubmitEditing={Keyboard.dismiss}
+            onChangeText={(text) => handeTextInput(text)}
+            placeholderTextColor={"#A4B3BF"}
+            placeholder="Search"
+            style={styles.textInput}
+            autoFocus={true}
+            maxLength={20}
+          ></TextInput>
+        ) : (
+          <Icon
+            color="#fff"
+            containerStyle={{}}
+            disabledStyle={{}}
+            iconProps={{}}
+            iconStyle={{}}
+            name="search"
+            onLongPress={() => console.log("onLongPress()")}
+            onPress={() => console.log(setSearch(true))}
+            size={30}
+            type="material"
+          ></Icon>
+        )}
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={data.filter((data) =>
+          data.name.toLowerCase().includes(searchTerm)
+        )}
+        refreshing={update}
+        renderItem={({ item }) => <Item coin={item} />}
+        onRefresh={async () => {
+          setUpdate(true);
+          await getData();
+          setUpdate(false);
+          Alert.alert("updated");
+        }}
+      ></FlatList>
     </View>
   );
 }
@@ -13,8 +87,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#112A40",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  list: {
+    width: "100%",
+    paddingHorizontal: 15,
+  },
+  title: {
+    fontSize: 30,
+
+    color: "#fff",
+  },
+  header: {
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    color: "#fff",
+    fontSize: 20,
   },
 });
---
+++ components/Item.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Text, View ,Image,StyleSheet} from 'react-native'
+
+const Item = ({coin}) => {
+    
+  return (
+    
+  <View style={styles.itemList} >
+    <View style={styles.logoName}>
+        <Image style={styles.logo} source={{uri:coin.image}} ></Image>
+        <Text style={styles.name} >{coin.name} </Text>
+        </View>
+   <View style={styles.data} >
+        <Text style={styles.price} >{coin.current_price} US$ </Text>
+        <Text style={[styles.percentage , coin.price_change_percentage_24h>0 ?styles.up :styles.down]} >{coin.price_change_percentage_24h} </Text>
+  </View>
+  </View>
+    
+
+  )
+}
+
+const styles= StyleSheet.create({
+    logoName:{
+        flexDirection:"row",
+        
+      
+        
+    },
+    logo:{
+        width:30,
+        height:30,
+        marginRight:10
+    },
+    name:{
+        
+        fontSize: 22,
+        color:"#fff"
+    },
+    itemList:{
+        marginVertical:10,
+        flexDirection:"row",
+        
+        justifyContent:"space-between"
+    },
+    price:{
+       
+        textAlign:"right",
+        color:"#fff"
+    },
+    percentage:{
+       
+        textAlign:"right",
+        
+    },
+    data:{
+    
+
+    },
+    up:{
+        color:"green"
+
+    },
+    down:{
+        color:"red"
+    },
+})
+
+export default Item
